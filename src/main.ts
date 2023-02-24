370511078b426a617287d34dd2c5c2215ccd56b2
Julien Lambert <julien.lambert@professorbob.ai>
2023-02-24 17:16:45 +0100

    feat: main script

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { common } from "./common";
+import { configureCodeArtifact } from "./lib/configure";
+import { parseInputs, validateInputs } from "./lib/inputs";
+import { fetchToken } from "./lib/token";
+
+async function main() {
+  const config = common();
+
+  const token = await fetchToken(config.registry, config.duration, config.aws);
+
+  configureCodeArtifact(config, token);
+}
+
+main().catch((e) => {
+  console.log("Error occurred");
+  e && console.log(e);
+  process.exit;
+});
